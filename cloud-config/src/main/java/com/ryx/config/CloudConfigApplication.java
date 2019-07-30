33bd9de8e1ece75d4bd5ead1f6f2a148513f9163
renyuxing1 <renyuxing66@163.com>
2019-07-30 21:53:57 +0800

    test

--- cloud-config/src/main/java/com/ryx/config/CloudConfigApplication.java
+++ cloud-config/src/main/java/com/ryx/config/CloudConfigApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableConfigServer
 @SpringBootApplication
 public class CloudConfigApplication {
-
+    //111SS
     public static void main(String[] args) {
         SpringApplication.run(CloudConfigApplication.class, args);
     }
