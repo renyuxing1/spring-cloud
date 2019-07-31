aaa3c0def94084770ca7e41dc50b112fb655f737
renyuxing <renyuxing@winchannel.net>
2019-07-31 17:29:07 +0800

    add something

--- system-server/src/main/java/com/ryx/system/server/service/StudentService.java
+++ system-server/src/main/java/com/ryx/system/server/service/StudentService.java
@@ -2,15 +2,20 @@ package com.ryx.system.server.service;
 
 import com.ryx.common.domain.model.Student;
 import com.ryx.common.feign.systemService.StudentServiceClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentService implements StudentServiceClient {
+
+    @Value("${test1.a}")
+    private Integer age;
+
     @Override
     public Student getStudentInfo() {
         Student student = new Student();
         student.setName("aaa");
-        student.setAge(11);
+        student.setAge(age);
         return student;
     }
 }
